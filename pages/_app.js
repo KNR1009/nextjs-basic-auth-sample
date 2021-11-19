05d89359bd210c400faa9c68cd668d01868417da
伊東和成 <itokazunari5375@icloud.com>
2021-11-19 14:46:25 +0900

    不要箇所の削除

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
 import App from "next/app";
 
-import basicAuthCheck from "../src/basicAuthCheck.ts";
+// import basicAuthCheck from "../src/basicAuthCheck.ts";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <h1>hello</h1>;
 }
 
-MyApp.getInitialProps = async (appContext) => {
-  const { req, res } = appContext.ctx;
-  if (req && res) {
-    await basicAuthCheck(req, res);
-  }
+// MyApp.getInitialProps = async (appContext) => {
+//   const { req, res } = appContext.ctx;
+//   if (req && res) {
+//     await basicAuthCheck(req, res);
+//   }
 
-  const appProps = await App.getInitialProps(appContext);
-  return { ...appProps };
-};
+//   const appProps = await App.getInitialProps(appContext);
+//   return { ...appProps };
+// };
 
 export default MyApp;
