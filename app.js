f9e21bd45a28334ec843bfe3efb4344908704c27
伊東和成 <itokazunari5375@icloud.com>
2021-11-19 14:28:03 +0900

    add console

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const USER_NAME = "user";
 const PASSWORD = "password";
 
 const app = protect(
-  "/",
-  (username, password) => {
-    return safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD);
-  },
-  {
-    directory: `${__dirname}/public`,
-    onAuthFailed: (res) => {
-      res.end("Authentication failed.");
-    },
-  }
+  console.log(app.js)
+  // "/",
+  // (username, password) => {
+  //   return safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD);
+  // },
+  // {
+  //   directory: `${__dirname}/public`,
+  //   onAuthFailed: (res) => {
+  //     res.end("Authentication failed.");
+  //   },
+  // }
 );
 
 module.exports = app;
