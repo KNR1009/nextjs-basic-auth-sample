69eae351376d41252ce11d033c6a108d9a88fd5a
伊東和成 <itokazunari5375@icloud.com>
2021-11-19 14:10:36 +0900

    add basic certification

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import App from "next/app";
+
+import basicAuthCheck from "../src/basicAuthCheck.ts";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.getInitialProps = async (appContext) => {
+  const { req, res } = appContext.ctx;
+  if (req && res) {
+    await basicAuthCheck(req, res);
+  }
+
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps };
+};
+
+export default MyApp;
