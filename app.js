1f5bc1efc7ff4e453a6207ac3c8749880c40ae90
伊東和成 <itokazunari5375@icloud.com>
2021-11-19 16:06:14 +0900

    add ip address

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const protect = require("static-auth");
-const safeCompare = require("safe-compare");
+const express = require("express");
+const requestIp = require("request-ip");
+const ipRangeCheck = require("ip-range-check");
+const is = require("is_js");
+const basicAuth = require("basic-auth-connect");
 
-const app = protect(
-  "/",
-  (username, password) =>
-    safeCompare(username, process.env.USERNAME || "admin") &&
-    safeCompare(password, process.env.PASSWORD || "admin"),
-  {
-    directory: `${__dirname}/out`,
-    onAuthFailed: (res) => {
-      res.end("Authentication failed");
-    },
-  }
-);
+// ::1がクライアントのipアドレスになっているので、これで許可される
+const ALLOWIPS = ["127.0.0.1", "xxx.xxx.xx.xxx", "::1"];
+const USERNAME = "user";
+const PASSWORD = "pass";
 
+const app = express();
+
+// IP Auth
+app.all("/*", (req, res, next) => {
+  var clientIp = is.ip(req.headers["fastly-client-ip"])
+    ? req.headers["fastly-client-ip"]
+    : requestIp.getClientIp(req);
+  ipRangeCheck(clientIp, ALLOWIPS) ? next() : res.send("Not Allowed");
+});
+
+app.use(basicAuth(USERNAME, PASSWORD));
+
+// app.use(express.static(__dirname + "/public/"));
 module.exports = app;
